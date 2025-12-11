9ef5e0b3f5e11b9041358737d93524bb75ca790d
J SHAKTHI SRAVANTH <121029982+shakthisravanth@users.noreply.github.com>
2025-12-11 17:54:19 +0530

    updated comments

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        //Write your code here
+        
+        // Initialize variables
+        
+
+        // Print the data with concatination
+
+        
     }
 }
